b7252116899c00b50cb97535f5679fad2025599f
Nghĩa Huỳnh Lê Nhất <nghiahln@techhaus.vn>
2023-08-16 14:23:55 +0700

    feat: define a class log_lib

--- applications/commons/log_lib.py
+++ applications/commons/log_lib.py
@@ -1,3 +1,4 @@
+import datetime
 import uuid
 import logging
 
@@ -65,3 +66,189 @@ class LogMessage(object):
             'NOTSET': 0
         }
         logger_obj.log(logger_level.get(level), message)
+
+    def log(self, lv, message, func_name="", ):
+        """
+        :param lv: level to log
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        log_message = {
+            'func_name': self.func_name if not func_name else func_name,
+            'message': message,
+            'user': self.user,
+            "traceback": "trace-id-{}".format(self.trace_id),
+            "time": f"{datetime.datetime.now()}"
+        }
+
+        my_adapter = logging.LoggerAdapter(self._logger,
+                                           {
+                                               'func_name': self.func_name if not func_name else func_name,
+                                               'user': self.user,
+                                               "traceback": "trace-id-{}".format(self.trace_id),
+                                               "time": f"{datetime.datetime.now()}"
+                                           })
+
+        # print(log_message)
+        self.logs_message(lv, log_message, my_adapter)
+
+    def info(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.log("info", message, func_name)
+
+    def error(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.log("error", message, func_name)
+
+    def warning(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.log("warning", message, func_name)
+
+    def debug(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        if settings.LOG_DEBUG:
+            self.log("info", message, func_name)
+
+    @classmethod
+    def init_log(cls, func_name, logger=None, user="", client_id="", service_name=""):
+        """
+        :param func_name: function name where we wrote log line
+        :param logger: logger object or logger name.
+        :param user: username to log
+        :param client_id: client_id to identify who call api
+        :param service_name: service name to identify what service logged
+        :return:
+        """
+        if not logger:
+            init_service_name = service_name if service_name else settings.SERVICE_NAME
+            log_message = cls(func_name=func_name, user=user, service_name=init_service_name)
+        elif isinstance(logger, cls):
+            user = logger.user
+            init_service_name = service_name if service_name else logger.service_name
+            log_message = cls(func_name=func_name, logger=logger.pub_logger, user=user, client_id=logger.client_id,
+                              trace_id=logger.trace_id, service_name=init_service_name)
+        elif isinstance(logger, MixingLog):
+            user = logger.default.user
+            init_service_name = service_name if service_name else logger.default.service_name
+            log_message = cls(func_name=func_name, logger=logger.default.pub_logger, user=user,
+                              client_id=logger.client_id, trace_id=logger.default.trace_id,
+                              service_name=init_service_name)
+        else:
+            init_service_name = service_name if service_name else settings.SERVICE_NAME
+            log_message = cls(func_name=func_name, logger=logger, user=user, client_id=client_id,
+                              service_name=init_service_name)
+        return log_message
+
+
+class MixingLog(object):
+    def __init__(self, func_name="", user=None, init_span=True, _parent_logger=None, client_id="", service_name=None):
+        self.func_name = func_name
+        self.user = user
+        self.default = None
+        self.client_id = client_id
+        self._parent_logger = _parent_logger
+        self.span = None
+        self.init_span = init_span
+        self.service_name = service_name if service_name else settings.SERVICE_NAME
+        self.init_log_base_settings()
+
+    @property
+    def trace_id(self):
+        """
+        This function to show trace id of logger into log line.
+        :return:
+        """
+        return self.default.trace_id if self.default else uuid.uuid1().hex
+
+    def set_user_log(self, username):
+        """
+        This func to set user into logger.
+        :param username: this username will be show in each log line.
+        :return:
+        """
+        self.user = username
+        if self.default:
+            self.default.set_user_log(username)
+
+    def init_log_base_settings(self):
+        """
+        This func to set init for log base setting.
+        :return:
+        """
+        if self.init_span:
+            if isinstance(self._parent_logger, (LogMessage, MixingLog)):
+                self.user = self._parent_logger.user
+                self.client_id = self._parent_logger.client_id
+        self.default = LogMessage(logger="", func_name=self.func_name, client_id=self.client_id, user=self.user,
+                                  service_name=self.service_name)
+        self.span = self.default.span
+        for i in settings.LOGGING["loggers"].keys():
+            if i:
+                setattr(self, i,
+                        LogMessage(logger=i, func_name=self.func_name, user=self.user, client_id=self.client_id,
+                                   service_name=self.service_name))
+
+    def set_component(self, value):
+        """
+        This function to set up component value.
+        :param value:
+        :return:
+        """
+        return self.default.set_component(value)
+
+    def info(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.default.info(message, func_name)
+
+    def error(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.default.error(message, func_name)
+
+    def warning(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.default.warning(message, func_name)
+
+    def log(self, lv, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.default.log(lv, message, func_name)
+
+    def debug(self, message, func_name=""):
+        """
+        :param message: message to log
+        :param func_name: function name where we wrote log line
+        :return:
+        """
+        self.default.debug(message, func_name)
