5c152d08854ca96af565f377e3264e8edd3a9b60
Nghĩa Huỳnh Lê Nhất <nghiahln@techhaus.vn>
2023-08-17 08:57:38 +0700

    feat: create RequestFetch

--- applications/commons/request_basic.py
+++ applications/commons/request_basic.py
@@ -1,3 +1,7 @@
+from applications.commons.log_lib import trace_func
+import requests
+
+
 class RequestFetch(object):
     POST = "post"
     GET = "get"
@@ -13,4 +17,45 @@ class RequestFetch(object):
         self.header = {} if not header else header
         self.url = "{}://{}".format(self.protocol, self.host_name)
 
+    def get_header(self, header=None):
+        default_header = {}
+        if self.gis:
+            default_header.update({
+                "gis": self.gis
+            })
+        if header:
+            default_header.update(header)
+        return default_header
 
+    @trace_func()
+    def fetch(self, uri='', body=None, files=None, logger=None, header=None, method="post", timeout=10, params=None,
+              log_resp=True, **kwargs):
+        if uri.startswith("/"):
+            full_url = "{}{}".format(self.url, uri) if uri else self.url
+        else:
+            full_url = "{}/{}".format(self.url, uri) if uri else self.url
+        func_call = getattr(requests, method)
+        header = self.get_header(header)
+        func_name = uri.replace("/", "_").replace("-", "_")
+        logger.debug("call {} with url {} body {}".format(func_name, full_url, body))
+        data = {
+            "result": False
+        }
+        body = body if body else {}
+        try:
+            response = func_call(full_url, json=body, headers=header, files=files, verify=self.cert, timeout=timeout,
+                                 params=params if params else {})
+            data.update(http_status=response.status_code)
+        except Exception as e:
+            logger.error(f"call {func_name} error with {e}")
+        else:
+            try:
+                data.update({"data": response.json()})
+                data["result"] = True
+            except Exception as e:
+                logger.error(f"parse data error with {response.content} and exception {e}")
+            else:
+                if log_resp:
+                    logger.debug('Call API {} SUCCESS: {}'.format(func_name, data))
+
+        return data
